f7b66147266ea35bcbaf7cf5bd480f0584e78749
Naveen <naveenkrishnawodeyar@gmail.com>
2024-10-05 20:06:53 +0530

    dairy project initial commit

---
+++ dairyEmployeeApp/src/main/java/com/gowri/DairyEmployeeAppApplication.java
@@ -0,0 +1,13 @@
+package com.gowri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DairyEmployeeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DairyEmployeeAppApplication.class, args);
+	}
+
+}
---
+++ dairyEmployeeApp/src/test/java/com/gowri/DairyEmployeeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gowri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DairyEmployeeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
