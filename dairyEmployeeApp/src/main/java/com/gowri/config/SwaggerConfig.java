259b71dc5ff058d7ec725302b0f6068c85c77608
Naveen <naveenkrishnawodeyar@gmail.com>
2024-10-08 22:22:44 +0530

    swagger error while configuring

--- dairyEmployeeApp/src/main/java/com/gowri/config/SwaggerConfig.java
+++ dairyEmployeeApp/src/main/java/com/gowri/config/SwaggerConfig.java
@@ -1,10 +1,10 @@
-package com.gowri.config;/*
+package com.gowri.config;
+
+import org.springframework.context.annotation.Configuration;
+/*
  * @author NaveenWodeyar
  * @date 07-10-2024
  */
-
-import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class SwaggerConfig {
 }
