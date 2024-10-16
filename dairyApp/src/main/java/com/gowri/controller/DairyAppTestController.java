fff2809a8348b76403007cfabc1180ecea4a244e
Naveen <naveenkrishnawodeyar@gmail.com>
2024-10-16 21:21:40 +0530

    port number added in test endPoint

--- dairyApp/src/main/java/com/gowri/controller/DairyAppTestController.java
+++ dairyApp/src/main/java/com/gowri/controller/DairyAppTestController.java
@@ -2,6 +2,8 @@ package com.gowri.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +18,13 @@ public class DairyAppTestController {
 
     private static final Logger log = LoggerFactory.getLogger(DairyAppTestController.class);
 
+    @Autowired
+    private Environment  environment;
+
     @GetMapping
     public String test(){
         log.warn("test passed");
-        return "DairyApp_connected,";
+        String port = environment.getProperty("server.port");
+        return "DairyApp_connected running on " + port;
     }
 }
--- dairyEmployeeApp/src/main/java/com/gowri/controller/EmpController.java
+++ dairyEmployeeApp/src/main/java/com/gowri/controller/EmpController.java
@@ -6,6 +6,8 @@ package com.gowri.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/emp")
 public class EmpController {
-
     public static final Logger log = LoggerFactory.getLogger(EmpController.class);
+
+    @Autowired
+    private Environment environment;
 
     @GetMapping
     public String testEndPoint(){
         log.info("test endpoint accessed");
-        return "Dairy Employee App connected,";
+        String port = environment.getProperty("server.port", "8080");
+        return "Dairy Employee App connected, running on port: " + port;
     }
 }
