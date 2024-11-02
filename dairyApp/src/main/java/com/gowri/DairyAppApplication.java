d20a75e2c3318c60d9390d22ac060c43e7115ca6
Naveen <naveenkrishnawodeyar@gmail.com>
2024-11-03 00:01:24 +0530

    Dockerfile added

--- dairyApp/src/main/java/com/gowri/DairyAppApplication.java
+++ dairyApp/src/main/java/com/gowri/DairyAppApplication.java
@@ -2,6 +2,10 @@ package com.gowri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class DairyAppApplication {
@@ -13,4 +17,20 @@ public class DairyAppApplication {
 		System.out.println(Thread.currentThread().getName());
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void getAppConfig(ApplicationReadyEvent 	event) {
+		ConfigurableEnvironment environment = event.getApplicationContext().getEnvironment();
+
+		// Get the active profile(s)
+		String[] activeProfiles = environment.getActiveProfiles();
+		String activeProfile = (activeProfiles.length > 0) ? String.join(", ", activeProfiles) : "default";
+
+		// Get the running port
+		String port = environment.getProperty("server.port", "8080"); // Default to 8080 if not set
+
+		System.out.println("Active Profile: " + activeProfile);
+		System.out.println("Running on Port: " + port);
+		System.out.println("#########################################");
+	}
 }
+
--- dairyEmployeeApp/src/main/java/com/gowri/DairyEmployeeAppApplication.java
+++ dairyEmployeeApp/src/main/java/com/gowri/DairyEmployeeAppApplication.java
@@ -2,6 +2,9 @@ package com.gowri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class DairyEmployeeAppApplication {
@@ -11,4 +14,20 @@ public class DairyEmployeeAppApplication {
 		System.out.println("#########################################");
 	}
 
+	// Listener to get the active profile and port after the application is ready
+	@EventListener(ApplicationReadyEvent.class)
+	public void handleApplicationReadyEvent(ApplicationReadyEvent event) {
+		Environment env = event.getApplicationContext().getEnvironment();
+
+		// Get the active profile(s)
+		String[] activeProfiles = env.getActiveProfiles();
+		String activeProfile = (activeProfiles.length > 0) ? String.join(", ", activeProfiles) : "default";
+
+		// Get the running port
+		String port = env.getProperty("server.port", "8080"); // Default to 8080 if not set
+
+		System.out.println("Active Profile: " + activeProfile);
+		System.out.println("Running on Port: " + port);
+		System.out.println("#########################################");
+	}
 }
